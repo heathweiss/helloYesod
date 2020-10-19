e778647268d182ab0b693453266225a8b127300d
Heath Weiss <HeathWeiss@hotmail.com>
2020-10-19 11:46:50 -0600

    updated comments

--- src/Handler/LinkToProject.hs
+++ src/Handler/LinkToProject.hs
@@ -17,7 +17,7 @@ getLinkToProjectR = defaultLayout $ do
   <ul>
    <li>All entries are done in this helloYesod project.
    <li>Added ../investingRIO to the stack.yaml packages field.
-   <li>Added investingRIO to the executable/library/tests/ build-depends, though should test if it is needed in all of these.
+   <li>Added investingRIO to the cabal file: executable/library/tests/ build-depends, though should test if it is needed in all of these.
    <li>If I edit a investingRIO file, without buiding in the investingRIO project, changes still show up in helloYesod, including not building with an error.
    <li>I am linked, to the temporary name fx in Roe.
    <li>
